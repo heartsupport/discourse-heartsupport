42d59bc9199b8ae36e8ec03878f7332d4a6f5eb8
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2022-10-19 19:23:42 +0300

    added category screening

--- plugin.rb
+++ plugin.rb
@@ -12,36 +12,39 @@ after_initialize do
       needs_support_tag = Tag.find_or_create_by(name: "Needs-Support")
       supported_tag = Tag.find_or_create_by(name: "Supported")
       supplier_url = URI("https://porter.heartsupport.com/webhooks/supplier")
+      supported_categories = ["66", "67", "87"]
 
-      if is_first_post?
-        # If it's the first post, add the needs support tag
-        topic.tags << needs_support_tag
-        # make an API call to create a supplier topic
-        res = Net::HTTP.post_form(supplier_url, topic_id: topic_id, supported: false, username: user.username, category_id: topic.category_id)
-      else
-        supported = !topic.tags.include?(needs_support_tag)
-        newly_supported = false
+      unless supported_categories.include?(topic.category_id)
+        if is_first_post?
+          # If it's the first post, add the needs support tag
+          topic.tags << needs_support_tag
+          # make an API call to create a supplier topic
+          res = Net::HTTP.post_form(supplier_url, topic_id: topic_id, supported: false, username: user.username, category_id: topic.category_id)
+        else
+          supported = !topic.tags.include?(needs_support_tag)
+          newly_supported = false
 
-        unless supported
-          # If it's not the first post and it has a needs support tag, check the support threshold and modify tags as necessary
+          unless supported
+            # If it's not the first post and it has a needs support tag, check the support threshold and modify tags as necessary
 
-          replies = topic.posts.where("post_number > 1")
-          reply_word_count = replies.sum(:word_count)
+            replies = topic.posts.where("post_number > 1")
+            reply_word_count = replies.sum(:word_count)
 
-          if (replies.length >= 1 && reply_word_count >= 500)
-            topic.tags.delete needs_support_tag
-            # topic.tags << supported_tag
-            supported = true
-            newly_supported = true
+            if (replies.length >= 1 && reply_word_count >= 500)
+              topic.tags.delete needs_support_tag
+              # topic.tags << supported_tag
+              supported = true
+              newly_supported = true
 
-            # make an API call to mark supplier topic as supported
-            res = Net::HTTP.post_form(supplier_url, topic_id: topic_id, supported: true, username: user.username, category_id: topic.category_id)
+              # make an API call to mark supplier topic as supported
+              res = Net::HTTP.post_form(supplier_url, topic_id: topic_id, supported: true, username: user.username, category_id: topic.category_id)
+            end
           end
-        end
 
-        Rails.logger.info("POSTING TO HSAPPS")
-        uri = URI("https://porter.heartsupport.com/twilio/discourse_webhook")
-        res = Net::HTTP.post_form(uri, topic_id: topic_id, supported: supported, newly_supported: newly_supported, body: cooked, username: user.username)
+          Rails.logger.info("POSTING TO HSAPPS")
+          uri = URI("https://porter.heartsupport.com/twilio/discourse_webhook")
+          res = Net::HTTP.post_form(uri, topic_id: topic_id, supported: supported, newly_supported: newly_supported, body: cooked, username: user.username)
+        end
       end
 
       Rails.logger.info("CHECKING FOR SUPPORT ON #{self}")
